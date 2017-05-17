ad09662f241a0328c8d4e3d7291087519aaea617
zivanovicb1 <zivanovic.b1@hotmail.com>
2017-05-18 00:11:55 +0200

    conditional transitioning

--- src/App.js
+++ src/App.js
@@ -26,12 +26,26 @@ class App extends Component {
   render() {
     return (
       <div className="App motions">
-        <Motion defaultStyle={{x: this._w+150, w:100}} style={{x: spring(0,presets.noWobble),w: spring(50,widthSpring)}}>
+        <Motion defaultStyle={{x: this._w+150, w:100}}
+          style={{x: spring(0,presets.noWobble),w: spring(50,widthSpring)}}>
           {value => {
             return (
-              <div style={{ transform: `translate(${value.x}px,0)`, width: `${value.w}%` }} className="blue">
+              <div style={{
+                transform: `translate(${value.x}px,0)`,
+                width: `${value.w}%`
+               }} className="blue">
                 <div className="container">
-                  <p>Join the tribe if you fit the job. <span>Work should never be work.</span></p>
+                  <Motion defaultStyle={{o:0, y:70 }} style={{o:spring(this.state.start ? 1 : 0,widthSpring), y:spring(this.state.start ? 0 : 70,widthSpring)}}>
+                    { value => {
+                      return(
+                        <p style={{
+                          opacity: value.o,
+                          transform: `translate(0,${value.y}px)`
+                        }}>Join the tribe if you fit the job. <span>Work should never be work.</span></p>
+                      )
+                    }}
+                  </Motion>
+
                 </div>
               </div>
             )
