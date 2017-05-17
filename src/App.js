e1eb68d33278505d013a6034ffa848ca6286ea61
zivanovicb1 <zivanovic.b1@hotmail.com>
2017-05-17 23:56:04 +0200

    opacity and transforms with simple values

--- src/App.js
+++ src/App.js
@@ -2,17 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Motion,spring,presets} from 'react-motion';
+
+const widthSpring = {
+  damping: 110,
+  stiffnes: 20
+}
+
 class App extends Component {
+  _w = screen.width;
+
+  state = {
+    start: false
+  };
+
+  constructor(props){
+    super(props);
+    setTimeout(() =>{
+      this.setState({start:true})
+    },500);
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App motions">
+        <Motion defaultStyle={{x: this._w+150, w:100}} style={{x: spring(0,presets.noWobble),w: spring(50,widthSpring)}}>
+          {value => {
+            return (
+              <div style={{ transform: `translate(${value.x}px,0)`, width: `${value.w}%` }} className="blue">
+                <div className="container">
+                  <p>Join the tribe if you fit the job. <span>Work should never be work.</span></p>
+                </div>
+              </div>
+            )
+          }}
+        </Motion>
+
+        <Motion defaultStyle={{o: 0}} style={{o: spring(this.state.start ? 1 : 0,widthSpring)}}>
+          {value => <div style={{opacity: `${value.o}`}} className="right"></div> }
+        </Motion>
       </div>
     );
   }
